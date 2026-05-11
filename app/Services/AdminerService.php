75a49c7e7595b3ccd9cff11065bd14b1232b86c7
David Courtey <courtey.d@gmail.com>
2026-05-11 12:27:31 +0200

    refactor: simplify AdminerService and fix package name
    - Remove dead code: serveStaticFile() and startOutputBuffering() were
      unused since no plugins load static assets and css() override prevents
      the default adminer.css link
    - Base64-encode session data to avoid MySQL utf8mb4 encoding errors
      with binary data in Adminer sessions
    - Rename package from dg/adminer-custom to dg/adminer (correct name)

--- app/Services/AdminerService.php
+++ app/Services/AdminerService.php
@@ -11,17 +11,12 @@ class AdminerService
      */
     public function render(?array $credentials): void
     {
-        $vendorAdminer = base_path('vendor/dg/adminer-custom');
+        $vendorAdminer = base_path('vendor/dg/adminer');
 
         $GLOBALS['_adminer_credentials'] = $credentials;
         $GLOBALS['_adminer_css_path'] = $this->resolveViteCssPath();
 
-        if ($this->serveStaticFile($vendorAdminer)) {
-            return;
-        }
-
         $this->useNonLockingSessionHandler();
-        $this->startOutputBuffering();
         $this->defineAdminerObject();
 
         chdir($vendorAdminer);
@@ -44,37 +39,6 @@ class AdminerService
         }
 
         return '/css/adminer.css';
-    }
-
-    /**
-     * Serve static files (JS, images) from the vendor package.
-     * Returns true if a file was served (and execution should stop).
-     */
-    private function serveStaticFile(string $vendorAdminer): bool
-    {
-        if (empty($_GET['file'])) {
-            return false;
-        }
-
-        if (! preg_match('#^(default|adminer|static(/\w[\w.-]*)+)\.(\w+)\z#', $_GET['file'], $m)) {
-            return false;
-        }
-
-        if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
-            header('HTTP/1.1 304 Not Modified');
-            exit;
-        }
-
-        header('Expires: '.gmdate('D, d M Y H:i:s', strtotime('1 month')).' GMT');
-        header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-
-        $types = ['css' => 'text/css', 'js' => 'text/javascript', 'gif' => 'image/gif', 'png' => 'image/png'];
-        if (isset($types[$m[3]])) {
-            header('Content-Type: '.$types[$m[3]]);
-        }
-
-        @readfile($vendorAdminer.'/'.$_GET['file']);
-        exit;
     }
 
     /**
@@ -106,12 +70,14 @@ class AdminerService
 
             public function read(string $id): string
             {
-                return (string) $this->cache->get('adminer_session:'.$id, '');
+                $value = $this->cache->get('adminer_session:'.$id);
+
+                return $value ? base64_decode($value) : '';
             }
 
             public function write(string $id, string $data): bool
             {
-                return $this->cache->put('adminer_session:'.$id, $data, $this->ttl);
+                return $this->cache->put('adminer_session:'.$id, base64_encode($data), $this->ttl);
             }
 
             public function destroy(string $id): bool
@@ -124,27 +90,6 @@ class AdminerService
                 return 0; // Cache TTL handles expiry
             }
         });
-    }
-
-    /**
-     * Start output buffering to strip the default adminer.css link tag
-     * (our Vite bundle replaces it) and rewrite static asset URLs.
-     */
-    private function startOutputBuffering(): void
-    {
-        define('ASSETS_VERSION', '1');
-
-        ob_start(function ($s) {
-            $s = preg_replace('#<link[^>]*href="[^"]*adminer\.css[^"]*"[^>]*/?\s*>#', '', $s);
-
-            return preg_replace_callback(
-                '#(<(link|script)\s[^>]*(href|src)=")(static/.+)(\?v=\d+)?"#U',
-                function ($m) {
-                    return $m[1].'?file='.urlencode($m[4]).'&amp;version='.ASSETS_VERSION.'"';
-                },
-                $s,
-            );
-        }, 4096);
     }
 
     /**
